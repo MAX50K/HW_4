18105eb93d7717ca1489657d0843c5b3b324a0a6
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-02-04 15:24:44 +0200

    save

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// 1
+let field1 = prompt("Заповніть рядок");
+let field2 = prompt("Заповніть рядок");
+let message = (field1 && field2) ? "Обидва поля заповнені" : "Не всі поля заповнені";
+alert("Check"); 
+console.log(message);
+// 2
+let number1 = prompt("Enter a number");
+let number2 = prompt("Enter another number");
+let otherNumber = 10
+let sum = (Number(number1) + Number(number2));
+let answer = (sum >= otherNumber) ? "The sum is over 10" : "The sum is under 10";
+alert("Check");
+console.log(answer);
+// 3
+let text = ("i like JavaScript")
+alert("Check")
+let answer3 = (text.includes("JavaScript"))
+if (answer3) {
+    console.log("The text includes Javascript");
+} else {
+    console.log("The text does not include Javasrcipt");
+}
+// 4
+let randomNumber = prompt("Enter a random number");
+let diapasonTrue = (randomNumber >= 10 && randomNumber <= 20)
+alert("Check");
+if (diapasonTrue){
+    console.log("The number is in the diapason of 10 to 20");
+} else {
+    console.log("The number is not in the diapason of 10 to 20");
+}
